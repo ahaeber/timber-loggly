5cea85f48b50c8d6b7f7c30330e2b79eedb114ed
Jagdeep Singh <jagdeep@rangde.org>
2017-05-02 17:52:49 +0530

    updated timber version

--- example/src/main/java/com/github/tony19/timber/loggly/example/MainActivity.java
+++ example/src/main/java/com/github/tony19/timber/loggly/example/MainActivity.java
@@ -17,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
         // Plant a LogglyTree for logging with Timber. Use tony19's demo
         // token for testing purposes only. Get your own token from
         // https://www.loggly.com/docs/customer-token-authentication-token/
-        final String LOGGLY_TOKEN = "1e29e92a-b099-49c5-a260-4c56a71f7c89";
+        final String LOGGLY_TOKEN = "2538a019-900f-4532-b4b3-906e7f82d3e4";
         Timber.plant(new LogglyTree(LOGGLY_TOKEN));
+        Timber.plant(new Timber.DebugTree());
 
         Timber.tag("timber-loggly demo");
         Timber.d("activity created");
